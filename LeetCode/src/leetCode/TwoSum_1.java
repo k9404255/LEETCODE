ab1fd9c7e893ddb653d14722f39f8dbd0f590c65
k9404 <k9404@host.docker.internal>
2023-12-10 14:12:15 +0800

    update

--- LeetCode/src/leetCode/TwoSum_1.java
+++ LeetCode/src/leetCode/TwoSum_1.java
@@ -1,5 +1,17 @@
 package leetCode;
 
 public class TwoSum_1 {
+	public static int[] twoSum(int[] nums, int target) {
+		return nums;
+	}
 
+	public static void main(String[] args) {
+		twoSum(new int[] { 2, 7, 11, 15 }, 9);
+		twoSum(new int[] { 3, 2, 4 }, 6);
+		twoSum(new int[] { 3,3 }, 6);
+
+		System.out.println(twoSum(new int[] { 2, 7, 11, 15 }, 9));
+		System.out.println(twoSum(new int[] { 3, 2, 4 }, 6));
+		System.out.println(twoSum(new int[] { 3,3 }, 6));
+	}
 }
