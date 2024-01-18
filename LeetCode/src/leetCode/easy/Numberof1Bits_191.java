f50220ded206256e74f2f1064daa8cc7c726bf6b
k9404 <k9404@Ryou.mshome.net>
2024-01-18 20:11:30 +0800

    update

---
+++ LeetCode/src/leetCode/easy/Numberof1Bits_191.java
@@ -0,0 +1,41 @@
+package leetCode.easy;
+
+public class Numberof1Bits_191 {
+	// solution 1: String
+	public static int hammingWeight1(int n) {
+		String s = Integer.toBinaryString(n);
+		s = s.replace("0", "");
+		return s.length();
+	}
+
+	// solution 2: bit count
+	public static int hammingWeight2(int n) {
+		return Integer.bitCount(n);
+	}
+
+	// solution 3: caculate
+	public static int hammingWeight3(int n) {
+		int count = 0;
+		if (n > 0) {
+			while (n != 0) {
+				count += n % 2;
+				n /= 2;
+			}
+		} else if (n < 0) {
+			n = ~n;
+			while (n != 0) {
+				count += n % 2;
+				n /= 2;
+			}
+			count = 32 - count;
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(hammingWeight1(-3));
+		System.out.println(hammingWeight2(-3));
+		System.out.println(hammingWeight3(-3));
+	}
+}
