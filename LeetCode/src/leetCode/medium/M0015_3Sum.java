e98fb8663cab613f9954b03330aecd200757adef
k9404 <k9404@Ryou.mshome.net>
2024-06-17 11:31:02 +0800

    update

--- LeetCode/src/leetCode/medium/M0015_3Sum.java
+++ LeetCode/src/leetCode/medium/M0015_3Sum.java
@@ -3,8 +3,10 @@ package leetCode.medium;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class M0015_3Sum {
 	// solution 1: brute-force (Time Limit Exceeded)
@@ -58,6 +60,41 @@ public class M0015_3Sum {
 		return new ArrayList<>(set);
 	}
 
+	// solution 3: based on twosum
+	public static List<List<Integer>> threeSum3(int[] nums) {
+		HashSet<List<Integer>> set = new HashSet<List<Integer>>();
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length; i++) {
+			int target = -nums[i];
+			List<List<Integer>> tmp = twoSum2(Arrays.copyOfRange(nums, i + 1, nums.length), target);
+			for (List<Integer> list : tmp) {
+				list.add(0, nums[i]);
+				set.add(list);
+			}
+		}
+
+		return new ArrayList<>(set);
+	}
+
+	public static List<List<Integer>> twoSum2(int[] nums, int target) {
+		HashSet<List<Integer>> set = new HashSet<List<Integer>>();
+
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+
+			if (map.containsKey(nums[i])) {
+				set.add(new ArrayList<>(Arrays.asList(nums[map.get(nums[i])], nums[i])));
+				// set.add(Arrays.asList(nums[map.get(nums[i])], nums[i]));
+			}
+
+			map.put(complement, i);
+		}
+
+		return new ArrayList<>(set);
+	}
+
 	public static void main(String[] args) {
 		System.out.println(threeSum1(new int[] { -1, 0, 1, 2, -1, -4 }));
 		System.out.println(threeSum1(new int[] { 0, 1, 1 }));
@@ -66,5 +103,9 @@ public class M0015_3Sum {
 		System.out.println(threeSum2(new int[] { -1, 0, 1, 2, -1, -4 }));
 		System.out.println(threeSum2(new int[] { 0, 1, 1 }));
 		System.out.println(threeSum2(new int[] { 0, 0, 0 }));
+
+		System.out.println(threeSum3(new int[] { -1, 0, 1, 2, -1, -4 }));
+		System.out.println(threeSum3(new int[] { 0, 1, 1 }));
+		System.out.println(threeSum3(new int[] { 0, 0, 0 }));
 	}
 }
